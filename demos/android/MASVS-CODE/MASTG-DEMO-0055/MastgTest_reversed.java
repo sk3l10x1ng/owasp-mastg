c502a0d9d899937da022f1da0581810a448ca5a6
Prudhvi Depuru <mr4obot@gmail.com>
2025-07-09 17:16:41 +0530

    updated for serializable

--- demos/android/MASVS-CODE/MASTG-DEMO-0055/MastgTest_reversed.java
+++ demos/android/MASVS-CODE/MASTG-DEMO-0055/MastgTest_reversed.java
@@ -1,19 +1,19 @@
 package org.owasp.mastestapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.util.Base64;
 import kotlin.Metadata;
 import kotlin.jvm.internal.Intrinsics;
 
 /* compiled from: MastgTest.kt */
-@Metadata(d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0012\n\u0002\b\u0003\b\u0007\u0018\u00002\u00020\u0001B\u000f\u0012\u0006\u0010\u0002\u001a\u00020\u0003¢\u0006\u0004\b\u0004\u0010\u0005J\u0006\u0010\u0006\u001a\u00020\u0007J\u0010\u0010\b\u001a\u0004\u0018\u00010\t2\u0006\u0010\n\u001a\u00020\u000bJ\u000e\u0010\f\u001a\u00020\u000b2\u0006\u0010\r\u001a\u00020\tR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004¢\u0006\u0002\n\u0000¨\u0006\u000e"}, d2 = {"Lorg/owasp/mastestapp/MastgTest;", "Ljava/io/Serializable;", "context", "Landroid/content/Context;", "<init>", "(Landroid/content/Context;)V", "mastgTest", "", "processSerializedData", "", "serializedData", "", "serializeObject", "obj", "app_debug"}, k = 1, mv = {2, 0, 0}, xi = 48)
+@Metadata(d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u000f\u0012\u0006\u0010\u0002\u001a\u00020\u0003¢\u0006\u0004\b\u0004\u0010\u0005J\u0006\u0010\u0006\u001a\u00020\u0007J\u000e\u0010\b\u001a\u00020\t2\u0006\u0010\n\u001a\u00020\u000bR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004¢\u0006\u0002\n\u0000¨\u0006\f"}, d2 = {"Lorg/owasp/mastestapp/MastgTest;", "", "context", "Landroid/content/Context;", "<init>", "(Landroid/content/Context;)V", "mastgTest", "", "processIntent", "", "intent", "Landroid/content/Intent;", "app_debug"}, k = 1, mv = {2, 0, 0}, xi = 48)
 /* loaded from: classes3.dex */
-public final class MastgTest implements Serializable {
+public final class MastgTest {
     public static final int $stable = 8;
     private final Context context;
 
@@ -23,28 +23,37 @@ public final class MastgTest implements Serializable {
     }
 
     public final String mastgTest() {
-        Log.d("MASTG-TEST", "Hello from the OWASP MASTG Test app.");
-        return "Hello from the OWASP MASTG Test app.";
-    }
-
-    public final Object processSerializedData(byte[] serializedData) {
-        Intrinsics.checkNotNullParameter(serializedData, "serializedData");
-        try {
-            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(serializedData));
-            return ois.readObject();
-        } catch (Exception e) {
-            Log.e("MASTG-TEST", "Deserialization error", e);
-            return null;
+        String status;
+        BaseUser user = UserManager.INSTANCE.getCurrentUser();
+        if ((user instanceof AdminUser) && ((AdminUser) user).getIsAdmin()) {
+            status = "PRIVILEGED ADMIN!";
+        } else {
+            status = "(Not an Admin)";
         }
+        String resultString = "Current User: " + user.getUsername() + "\nStatus: " + status + "\n\nVulnerability: Unwanted Object Deserialization is active.\nThe app will deserialize any 'BaseUser' subclass from the 'payload_b64' extra, overwriting the current user state.";
+        Log.d("MASTG-TEST", resultString);
+        return resultString;
     }
 
-    public final byte[] serializeObject(Object obj) {
-        Intrinsics.checkNotNullParameter(obj, "obj");
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(obj);
-        byte[] byteArray = baos.toByteArray();
-        Intrinsics.checkNotNullExpressionValue(byteArray, "toByteArray(...)");
-        return byteArray;
+    public final void processIntent(Intent intent) throws ClassNotFoundException, IOException {
+        Intrinsics.checkNotNullParameter(intent, "intent");
+        if (intent.hasExtra("payload_b64")) {
+            String b64Payload = intent.getStringExtra("payload_b64");
+            Log.d("VULN_APP", "Received a base64 payload. Deserializing user object...");
+            try {
+                byte[] serializedPayload = Base64.getDecoder().decode(b64Payload);
+                ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(serializedPayload));
+                Object untrustedObject = ois.readObject();
+                ois.close();
+                if (untrustedObject instanceof BaseUser) {
+                    UserManager.INSTANCE.setCurrentUser((BaseUser) untrustedObject);
+                    Log.i("VULN_APP", "User state overwritten with deserialized object!");
+                } else {
+                    Log.w("VULN_APP", "Deserialized object was not a user. State unchanged.");
+                }
+            } catch (Exception e) {
+                Log.e("VULN_APP", "Failed to deserialize payload", e);
+            }
+        }
     }
 }
